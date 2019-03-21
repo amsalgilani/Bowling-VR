6b78908fe6bc1b7fb3b7b73a8fca6f64dc1eb62b
amsalgilani <38839159+amsalgilani@users.noreply.github.com>
2019-03-21 16:16:18 -0400

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameController : MonoBehaviour
+{
+    public Player player;
+    public TextMesh infoText;
+    public Pin[] pins;
+
+    public float evaluationTime = 10f;
+    private float gameTimer = 0f;
+    private bool evaluating = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        infoText.text = "Throw the bowling ball!";
+        if (evaluating == false)
+        {
+            if (player.holding == false)
+            {
+                evaluating = true;
+                gameTimer = evaluationTime;
+            }
+        } else
+        {
+            gameTimer -= Time.deltaTime;
+            if (gameTimer <= 0f)
+            {
+                int score = 0;
+
+                for (int i = 0; i<pins.Length; i++)
+                {
+                    if (pins[i] == null)
+                    {
+                        score++;
+                    }
+                }
+                infoText.text = "Your score: " + score;
+            }
+            if (gameTimer <= -3f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bowlingBall;
+    public float ballDistance = 2.5f;
+    public float ballThrowingForce = 200f;
+
+    public bool holding = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (holding)
+        {
+            bowlingBall.transform.position = transform.position + transform.forward * ballDistance;
+                if (GvrViewer.Instance.Triggered || Input.GetKeyDown("space"))
+            {
+                    holding = false;
+                    bowlingBall.GetComponent<Rigidbody>().useGravity = true;
+                    bowlingBall.GetComponent<Rigidbody>().AddForce(transform.forward * ballThrowingForce);
+            }
+        }
+        
+    }
+}
